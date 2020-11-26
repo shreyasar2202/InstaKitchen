bd0706480dce34788ff243dc2e964df26bc38304
Vindhya N <vindhya@Vindhyas-MacBook-Pro.local>
2020-11-26 22:28:59 +0530

    removed relative view from SpaceGrid

--- app/src/main/java/com/example/kitchen/instakitchen/SpiceGrid.java
+++ app/src/main/java/com/example/kitchen/instakitchen/SpiceGrid.java
@@ -19,37 +19,13 @@ public class SpiceGrid extends AppCompatActivity {
         GridView spicesGrid;
         int[] spice_image_resource = {R.drawable.black_pepper, R.drawable.cardamom, R.drawable.cinnamon, R.drawable.clove, R.drawable.coriander_seeds, R.drawable.cumin};
         String[] spice_names = getResources().getStringArray(spices);
-        //RelativeLayout reLay = (RelativeLayout) findViewById(R.id.relative_gridLayout);
-
-        /*ArrayList<ProgressBar> quantity_bar = new ArrayList<ProgressBar>();
-        SpicesAdapter spiceAdapter;
-        for (int i = 0; i < 6; i++) {
-            ProgressBar quantity_obj = new android.widget.ProgressBar(getApplicationContext(), null, android.R.attr.progressBarStyleHorizontal);
-            int progress = (int) java.lang.Math.ceil(100 * java.lang.Math.random());
-            quantity_obj.setProgress(progress);
-            //     quantity_obj.getIndeterminateDrawable().setColorFilter(0xFFFF0000, android.graphics.PorterDuff.Mode.MULTIPLY);
-            quantity_bar.add(quantity_obj);
-//            (quantity_bar.get(i)).setProgress(prog);
-        }
-        */
 
         GridSpicesAdapter spiceAdapter;
         int i = 0;
 
-//        RelativeLayout my_relative = (RelativeLayout) findViewById(R.id.relative_layout);
-
-       /* DisplayMetrics displayMetrics=getResources().getDisplayMetrics();
-        int screen_width=displayMetrics.widthPixels;    //width of the device screen
-        int screen_height=displayMetrics.heightPixels;   //height of device screen
-
-        int view_width=screen_width/3;   //width for relativeview
-        int view_height=screen_height/2;   //height for relativeview
-        */
         spiceAdapter = new GridSpicesAdapter(getApplicationContext(), R.layout.grid_layout);
         spicesGrid = (GridView) findViewById(R.id.spicesGridView);
 
-//        spicesGrid.getLayoutParams().width = screen_width;
-//        spicesGrid.getLayoutParams().height = screen_height;
         spicesGrid.setAdapter(spiceAdapter);
 
         for (String spices : spice_names) {
